202b9ec8b0bb4960eae70b16c0f0b9f73d62f44d
NikChief <niki3000-90@mail.ru>
2022-04-07 22:31:19 +0500

    Some fixes

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -16,7 +16,7 @@ router
     const { email, password } = req.body;
     const user = await User.findOne({ where: { user_email: email } });
     // console.log(`${password}`, `has${user.password}`);
-    if (user && (await bcrypt.compare(password, user.password))) {
+    if (user && (await bcrypt.compare(password, user.user_password))) {
       req.session.uid = user.id;
       res.redirect('/');
     } else {
