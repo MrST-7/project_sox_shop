943d4ba4b146efacd2df40313cf7574ef566ec54
Den <ndvofc@gmail.com>
2022-04-07 20:21:28 +0300

    first

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const config = require('./config/config')
+const constructorRouter = require('./routes/constructor.routes')
 
 const app = express();
+
+config(app)
+
+app.use('/constructor', constructorRouter)
 
 app.listen(3000, () => console.log('Started at port 3000'));
--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
-const logger = require('logger');
+
 
 const config = (app) => {
   // нфиг для сессий отсутствует
   app.set('view engine', 'hbs');
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(logger('dev'));
+  app.use(express.static(path.join(process.env.PWD, 'public')));
+
 };
 module.exports = config;
