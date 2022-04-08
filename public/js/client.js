faa9b2b419392ccb0823b91c0f0f77d50b54cd45
vasily <moonalty@gmail.com>
2022-04-08 13:23:24 +0300

    some dinner fixes

--- public/js/client.js
+++ public/js/client.js
@@ -1,15 +1,19 @@
-const { post } = require("../../routes/regist.routes");
-
 const add = document.querySelector("#add");
 const update = document.querySelector("#update");
 const deletes = document.querySelector("#delete");
-
-// add.addEventListener("click", async (event) => {
-//   event.preventDefault();
-//   const response = await fetch("/notApproved", {
-//     method: "post",
-//     body: JSON.stringify({
-//       sock_id,
-//     }),
-//   });
-// });
+console.log(add);
+add.addEventListener("click", async (event) => {
+  event.preventDefault();
+  const action = event.target.href;
+  console.log(action);
+  const response = await fetch(action, {
+    method: "post",
+    body: JSON.stringify({
+      action,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      accept: "json",
+    },
+  });
+});
--- routes/notApproved.routes.js
+++ routes/notApproved.routes.js
@@ -5,25 +5,35 @@ const { Sock, User, Sock_season, Sock_size } = require("../db/models");
 // router.route("/").get((req, res) => res.render("admin"));
 router.route("/").get(async (req, res) => {
   let sock;
-  console.log(Sock);
   try {
     sock = await Sock.findAll({
       raw: true,
       where: {
         sock_isApproved: false,
       },
-      // order: ['id', 'DESC'],
     });
-    // const users  = await User.findAll({raw: true
-    //   // order: ['id', 'DESC'],
-    // })
-    // console.log(socks, users)npm
   } catch (error) {
     return res.render("error", {
       message: error,
     });
   }
   res.render("notApproved", { sock });
+});
+
+router.post("/notApproved/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const sock = await Sock.update({
+    sock_isApproved: true,
+    where: {
+      id,
+    },
+  });
+  try {
+    res.redirect("notApproved");
+  } catch (error) {
+    res.send("error");
+  }
 });
 
 module.exports = router;
