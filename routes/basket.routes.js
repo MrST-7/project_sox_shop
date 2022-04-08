b6104bc102c565a6a004074b58f3079ad54ccc71
MrST-7 <strukhachev7@gmail.com>
2022-04-08 12:53:12 +0300

    yo

--- routes/basket.routes.js
+++ routes/basket.routes.js
@@ -1,16 +1,4 @@
 const router = require('express').Router();
 const { Basket } = require('../db/models');
 
-// router.route('/')
-//   .get(async (req, res) => {
-//     const { uid } = req.session;
-//     const basketId = await Basket.findAll({ where: { id_user: uid }, raw: true });
 
-//     res.render('basket', basketId);
-//   })
-//   .get(async (req, res) => {
-//     const { uid } = req.session;
-//     const basketId = await Basket.findAll({ where: { id_user: uid }, raw: true });
-
-//     res.render('basket', basketId);
-//   });
--- routes/main.routes.js
+++ routes/main.routes.js
@@ -7,22 +7,6 @@ const {
 router
   .route('/')
   .get(async (req, res) => {
-    // let sock;
-    // console.log(Sock);
-    // try {
-    //   sock = await Sock.findAll({
-    //     raw: true,
-    //     // order: ['id', 'DESC'],
-    //   });
-    //   // const users  = await User.findAll({raw: true
-    //   //   // order: ['id', 'DESC'],
-    //   // })
-    //   // console.log(socks, users)npm
-    // } catch (error) {
-    //   return res.render('error', {
-    //     message: error,
-    //   });
-    // }
     res.render('showAllSocks', { /* sock */ });
   });
 
